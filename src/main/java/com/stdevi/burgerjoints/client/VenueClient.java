e0aa310bcebac867cd526224d826a34444541094
stdevi <dvstas00@gmail.com>
2020-09-19 16:42:01 +0300

    Add VenueClient.searchVenuesPhotos method

--- src/main/java/com/stdevi/burgerjoints/client/VenueClient.java
+++ src/main/java/com/stdevi/burgerjoints/client/VenueClient.java
@@ -1,6 +1,8 @@
 package com.stdevi.burgerjoints.client;
 
-import com.stdevi.burgerjoints.model.VenuesSearchResponse;
+import com.stdevi.burgerjoints.model.photo.PhotoResponse;
+import com.stdevi.burgerjoints.model.venue.VenuesSearchResponse;
+import com.stdevi.burgerjoints.utilities.ClientException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -17,6 +19,9 @@ public class VenueClient {
     @Value("${foursquare.client.clientSecret}")
     private String clientSecret;
 
+    @Value("${foursquare.client.oauth_token}")
+    private String oauth_token;
+
     @Value("${foursquare.api.search.category}")
     private String categoryId;
 
@@ -28,16 +33,33 @@ public class VenueClient {
     }
 
     public Mono<VenuesSearchResponse> searchVenuesNear(String place) {
-        return this.webClient.get()
-                .uri(uriBuilder -> uriBuilder
-                        .path("/search")
-                        .queryParam("near", place)
-                        .queryParam("categoryId", categoryId)
-                        .queryParam("v", v)
-                        .queryParam("client_id", clientId)
-                        .queryParam("client_secret", clientSecret)
-                        .build())
+        return this.webClient.get().uri(uriBuilder -> uriBuilder
+                .path("/search")
+                .queryParam("near", place)
+                .queryParam("categoryId", categoryId)
+                .queryParam("v", v)
+                .queryParam("client_id", clientId)
+                .queryParam("client_secret", clientSecret)
+                .build())
                 .retrieve()
+                .onStatus(httpStatus -> httpStatus.is4xxClientError() || httpStatus.is5xxServerError(), clientResponse ->
+                        clientResponse.bodyToMono(VenuesSearchResponse.class).flatMap(error ->
+                                Mono.error(new ClientException(error.getMeta().getErrorDetail(), error.getMeta().getHttpStatus()))))
                 .bodyToMono(VenuesSearchResponse.class);
+    }
+
+    public Mono<PhotoResponse> searchVenuesPhotos(String venueId) {
+        return this.webClient.get().uri(uriBuilder -> uriBuilder
+                .path(String.format("/%s/photos", venueId))
+                .queryParam("v", v)
+                .queryParam("client_id", clientId)
+                .queryParam("client_secret", clientSecret)
+                .queryParam("oauth_token", oauth_token)
+                .build())
+                .retrieve()
+                .onStatus(httpStatus -> httpStatus.is4xxClientError() || httpStatus.is5xxServerError(), clientResponse ->
+                        clientResponse.bodyToMono(VenuesSearchResponse.class).flatMap(error ->
+                                Mono.error(new ClientException(error.getMeta().getErrorDetail(), error.getMeta().getHttpStatus()))))
+                .bodyToMono(PhotoResponse.class);
     }
 }
